1f601a563bf404a3d1f56916d731df32b68d30c4
Doha-Mohamed <141078681+Doha-Mohamed@users.noreply.github.com>
2023-08-04 02:58:11 +0200

    Create script.js

---
+++ src/script.js
@@ -0,0 +1,500 @@
+const products=[
+    {
+        id:1,
+        name:"Pinopsida",
+        img: ("imgMostafa/garden/(1).jfif"),
+        price:35,
+        category:{garden:"largeGarden"},
+        rate:4
+    }
+
+    ,
+    {
+        id:2,
+        name:"byta Flower",
+        img: ("imgMostafa/Flowers/(1).jpg"),
+        price:40,
+        category:{Flower:"largeGarden"},
+        rate:3
+    }
+
+    ,
+    {
+        id:3,
+        name:"Abies alba",
+        img: ("imgMostafa/garden/(1).jpeg"),
+        price:55,
+        category:{Flower:"largeGarden"},
+        rate:5
+    }
+
+    ,
+    {
+        id:4,
+        name:"Vivendel",
+        img: ("imgMostafa/Flowers/(1).jpg"),
+        price:40,
+        category:{Flower:"largeGarden"},
+        rate:3
+    }
+
+    ,
+    {
+        id:5,
+        name:"Larkin Sunset Gardens",
+        img: ("imgMostafa/garden/(1).webp"),
+        price:40,
+        category:{garden:"largeGarden"},
+        rate:3
+    }
+    
+    ,
+    {
+        id:6,
+        name:"Weed Wizards",
+        img: ("imgMostafa/garden/(10).jpeg"),
+        price:40,
+        category:{garden:"largeGarden"},
+        rate:6
+    }
+    
+    ,
+    {
+        id:7,
+        name:"Stor nøkkerose",
+        img: ("imgMostafa/Flowers/(1).jpg"),
+        price:40,
+        category:{Flower:"largeGarden"},
+        rate:3
+    }
+    
+    ,
+    {
+        id:8,
+        name:"Rosy",
+        img: ("imgMostafa/pot/(10).jpg"),
+        price:40,
+        category:{pot:"largeGarden"},
+        rate:2
+    }
+    
+    ,
+    {
+        id:9,
+        name:"Molte",
+        img: ("imgMostafa/Flowers/(1).jpg"),
+        price:40,
+        category:{Flower:"largeGarden"},
+        rate:4
+    }
+    
+    ,
+    {
+        id:10,
+        name:"Vetgarden",
+        img: ("imgMostafa/garden/(10).jpg"),
+        price:40,
+        category:{garden:"largeGarden"},
+        rate:1
+    }
+    
+    ,
+    {
+        id:11,
+        name:"Well Weeded",
+        img: ("imgMostafa/garden/(11).jpeg"),
+        price:42,
+        category:{Flower:"largeGarden"},
+        rate:3
+    }
+    
+    ,
+    {
+        id:12,
+        name:"Geitrams",
+        img: ("imgMostafa/Flowers/(1).jpg"),
+        price:55,
+        category:{Flower:"largeGarden"},
+        rate:2
+    }
+    
+    ,
+    {
+        id:13,
+        name:"potrani",
+        img: ("imgMostafa/pot/(102).jpg"),
+        price:40,
+        category:{pot:"largeGarden"},
+        rate:3
+    }
+    
+    ,
+    {
+        id:14,
+        name:"Kusymre",
+        img: ("imgMostafa/Flowers/(100).jpg"),
+        price:35,
+        category:{Flower:"largeGarden"},
+        rate:5
+    }
+    
+    ,
+    {
+        id:15,
+        name:"Nrjes",
+        img: ("imgMostafa/pot/(102).jpg"),
+        price:43,
+        category:{pot:"largeGarden"},
+        rate:4
+    }
+    
+    ,
+    {
+        id:16,
+        name:"Marisko",
+        img: ("imgMostafa/Flowers/(104).jpg"),
+        price:40,
+        category:{Flower:"largeGarden"},
+        rate:3
+    }
+    
+    ,
+    {
+        id:17,
+        name:"Rødsildre",
+        img: ("imgMostafa/Flowers/(105).jpg"),
+        price:30,
+        category:{Flower:"largeGarden"},
+        rate:2
+    }
+    
+    ,
+    {
+        id:18,
+        name:"Landscape Lessons",
+        img: ("imgMostafa/garden/(13).jpg"),
+        price:33,
+        category:{garden:"largeGarden"},
+        rate:2
+    }
+    
+    ,
+    {
+        id:19,
+        name:"Mogop",
+        img: ("imgMostafa/Flowers/(107).jpg"),
+        price:41,
+        category:{Flower:"largeGarden"},
+        rate:5
+    }
+    
+    ,
+    {
+        id:20,
+        name:"Hgar",
+        img: ("imgMostafa/pot/(11).jpg"),
+        price:71,
+        category:{pot:"largeGarden"},
+        rate:4
+    }
+    
+    ,
+    {
+        id:21,
+        name:"Botanical Building",
+        img: ("imgMostafa/garden/(14).jpg"),
+        price:50,
+        category:{Flower:"largeGarden"},
+        rate:5
+    }
+    
+    ,
+    {
+        id:22,
+        name:"garden",
+        img: ("imgMostafa/Flowers/(1).jpg"),
+        price:41,
+        category:{Flower:"largeGarden"},
+        rate:4
+    }
+    
+    ,
+    {
+        id:23,
+        name:"Bakkekløver",
+        img: ("imgMostafa/Flowers/(1).jpg"),
+        price:40,
+        category:{Flower:"largeGarden"},
+        rate:3
+    }
+    
+    ,
+    {
+        id:24,
+        name:"Klokkelyng",
+        img: ("imgMostafa/Flowers/(1).jpg"),
+        price:29,
+        category:{Flower:"largeGarden"},
+        rate:1
+    }
+    
+    ,
+    {
+        id:25,
+        name:"Reinrose",
+        img: ("imgMostafa/Flowers/(1).jpg"),
+        price:60,
+        category:{Flower:"largeGarden"},
+        rate:3
+    }
+    
+    ,
+    {
+        id:26,
+        name:"Infinite Green",
+        img: ("imgMostafa/garden/(15).jpg"),
+        price:40,
+        category:{Flower:"largeGarden"},
+        rate:7
+    }
+    
+    ,
+    {
+        id:27,
+        name:"Super Seeds",
+        img: ("imgMostafa/Flowers/(16).jpg"),
+        price:37,
+        category:{Flower:"largeGarden"},
+        rate:3
+    }
+    
+    ,
+    {
+        id:28,
+        name:"	Søstermarihand",
+        img: ("imgMostafa/Flowers/(110).jpg"),
+        price:44,
+        category:{Flower:"largeGarden"},
+        rate:4
+    }
+    
+    ,
+    {
+        id:29,
+        name:"Ballblom",
+        img: ("imgMostafa/Flowers/(111).jpg"),
+        price:40,
+        category:{Flower:"largeGarden"},
+        rate:2
+    }
+    
+    ,
+    {
+        id:30,
+        name:"Sommereik",
+        img: ("imgMostafa/Flowers/(112).jpg"),
+        price:55,
+        category:{Flower:"largeGarden"},
+        rate:5
+    }
+    
+    ,
+    {
+        id:31,
+        name:"garden",
+        img: ("imgMostafa/Flowers/(1).jpg"),
+        price:41,
+        category:{Flower:"largeGarden"},
+        rate:3
+    }
+    
+    ,
+    {
+        id:32,
+        name:"Seeds for Weeds",
+        img: ("imgMostafa/pot/(114).jpg"),
+        price:40,
+        category:{pot:"largeGarden"},
+        rate:4
+    }
+    
+    ,
+    {
+        id:33,
+        name:"Liljekonvall",
+        img: ("imgMostafa/Flowers/(129).jpg"),
+        price:62,
+        category:{Flower:"largeGarden"},
+        rate:2
+    }
+    
+    ,
+    {
+        id:34,
+        name:"Lakeview Memorial",
+        img: ("imgMostafa/garden/(6).jpg"),
+        price:40,
+        category:{garden:"largeGarden"},
+        rate:3
+    }
+    
+    ,
+    {
+        id:35,
+        name:"Weed Free",
+        img: ("imgMostafa/pot/(120).jpg"),
+        price:30,
+        category:{pot:"largeGarden"},
+        rate:2
+    }
+    
+    ,
+    {
+        id:36,
+        name:"Bulb Babes",
+        img: ("imgMostafa/garden/(4).jpeg"),
+        price:50,
+        category:{Flower:"largeGarden"},
+        rate:5
+    }
+    
+    ,
+    {
+        id:37,
+        name:"Better Bulbs",
+        img: ("imgMostafa/Flowers/(111).jpg"),
+        price:30,
+        category:{Flower:"largeGarden"},
+        rate:5
+    }
+    ,
+
+    {
+        id:38,
+        name:"Baby Bulbs",
+        img: ("imgMostafa/garden/(5).jfif"),
+        price:20,
+        category:{Flower:"Garden"},
+        rate:3
+    }
+    ,
+    {
+        id:39,
+        name:"The Nursery",
+        img: ("imgMostafa/Flowers/(94).jpg"),
+        price:80,
+        category:{Flower:"largeGarden"},
+        rate:2
+    }
+    ,
+    {
+        id:40,
+        name:"The Flower Bed",
+        img: ("imgMostafa/pot/(34).jpg"),
+        price:58,
+        category:{pot:"largeGarden"},
+        rate:3
+    }
+    ,
+    {
+        id:41,
+        name:"Life of A Gardener",
+        img: ("imgMostafa/garden/(7).jfif"),
+        price:50,
+        category:{Flower:"largeGarden"},
+        rate:5
+    }
+    ,
+    {
+        id:42,
+        name:"Green Gardening",
+        img: ("imgMostafa/Flowers/(14).jpg"),
+        price:100,
+        category:{Flower:"largeGarden"},
+        rate:1
+    }
+    ,
+    {
+        id:42,
+        name:"Green Machine",
+        img: ("imgMostafa/Flowers/(148).jpg"),
+        price:40,
+        category:{Flower:"largeGarden"},
+        rate:3
+    }
+    ,
+    {
+        id:43,
+        name:"Garden Style",
+        img: ("imgMostafa/pot/(116).jpg"),
+        price:50,
+        category:{pot:"largeGarden"},
+        rate:2
+    }
+    ,
+    {
+        id:44,
+        name:"Blåveis",
+        img: ("imgMostafa/Flowers/(1).jpg"),
+        price:35,
+        category:{Flower:"Garden"},
+        rate:3
+    }
+    ,
+    {
+        id:45,
+        name:"Kew Gardens",
+        img: ("imgMostafa/garden/(7).jpg"),
+        price:65,
+        category:{garden:"largeGarden"},
+        rate:4
+    }
+    ,
+    {
+        id:46,
+        name:"Amaryllis",
+        img: ("imgMostafa/pot/(99).jpg"),
+        price:88,
+        category:{pot:"smallPlant"},
+        rate:1
+    }
+    ,
+    {
+        id:47,
+        name:"Bonnet House",
+        img: ("imgMostafa/garden/(8).jpg"),
+        price:90,
+        category:{garden:"gardenHome"},
+        rate:2
+    }
+    
+    ,
+    {
+        id:48,
+        name:"Seed Island",
+        img: ("imgMostafa/garden/(9).jpg"),
+        price:36,
+        category:{garden:"largeGarden"},
+        rate:5
+    }
+    ,
+    {
+        id:49,
+        name:"Azalea",
+        img: ("imgMostafa/Flowers/(99).jpg"),
+        price:50,
+        category:{Flower:"smallflower"},
+        rate:5
+    }
+    ,
+    {
+        id:50,
+        name:"Botanic Heaven",
+        img: ("imgMostafa/garden/(9).jpeg"),
+        price:26,
+        category:{garden:"largeGarden"},
+        rate:5
+    }
+
+
+]
